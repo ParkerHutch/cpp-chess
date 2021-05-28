15243094d61f7d1305884df8cd2195f9d09996ba
Parker Hutchinson <parker.hutch@outlook.com>
2021-05-28 13:59:31 -0400

    cmake project configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <SFML/Graphics.hpp>
 
+const sf::Vector2f windowDimensions(800, 800);
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(windowDimensions.x, windowDimensions.y),
+                            "Chess++");
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen()) {
-        
+
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
         }
 
         window.clear();
